fda4772fd055ce9768c0926a6a2cedd8cb156906
MaxHuzarevich <pinsk.maks@mail.ru>
2023-06-14 15:59:36 +0300

    - create style for modal

--- scripts/getLogin.js
+++ scripts/getLogin.js
@@ -1,3 +1,18 @@
-export const getLogin = () => {
+import {API_URL} from "./const.js";
 
+
+export const getLogin = async (token) => {
+    try {
+        const response = await fetch(`${API_URL}/getLogin`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+        });
+
+        return await response.json();
+    } catch (e) {
+        console.error(e)
+    }
 }
--- scripts/renderNavigation.js
+++ scripts/renderNavigation.js
@@ -19,7 +19,7 @@ export const renderNavigation = () => {
     buttonSignUp.addEventListener('click', () => {
         renderModal({
             title: 'Регистрация',
-            description: 'Введите ваши данные для регистрациина сервисе WishList',
+            description: 'Введите ваши данные для регистрации на сервисе WishList',
             btnSubmit: 'Зарегистрироваться',
             submitHandler: async (event) => {
                 const formData = new FormData(event.target);
@@ -58,7 +58,39 @@ export const renderNavigation = () => {
     });
 
     buttonLogin.addEventListener('click', () => {
-        console.log('buttonLogin')
+        renderModal({
+            title: 'Авторизация',
+            description: 'Введите ваши данные для входа в личный кабинет',
+            btnSubmit: 'Авторизоваться',
+            submitHandler: async (event) => {
+                const formData = new FormData(event.target);
+                const credentials = {
+                    login: formData.get('login'),
+                    password: formData.get('password'),
+                };
+
+                try {
+                    const response = await fetch(`${API_URL}/login`, {
+                        method: 'POST',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify(credentials)
+                    });
+                    if (response.ok) {
+                        const data = await resp.json()
+                        localStorage.setItem(JWT_TOKEN_KEY, data.token);
+                        auth.login = data.login;
+                        router.setRoute(`/user/${data.login}`);
+                        return true;
+                    } else {
+                        const {message = 'Неизвестная ошибка'} = await response.json();
+                        console.log(message)
+                        throw new Error(message)
+                    }
+                } catch (e) {
+                    alert(e.message)
+                }
+            }
+        })
     })
 
     nav.append(buttonSignUp, buttonLogin);
